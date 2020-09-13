d5a87bea0e6e66b851204802abeca596e6f461b8
dozer <diz.dozer@gmail.com>
2020-09-13 22:24:33 +0300

    wip

--- src/client/index.js
+++ src/client/index.js
@@ -1,2 +1,2 @@
-const Item = ({color, name, idx}) => <div>Hello<b>{idx}</b><b>{color}</b><span>{name}</span><h1>fi33ne</h1></div>
+const Item = ({color, name, idx}) => <div>Hello<b>{idx}</b><b>{color}</b><span>{name}</span></div>
 export const App = props => Array.from({length: 100}, (i, idx) => Item({...props, idx}))
