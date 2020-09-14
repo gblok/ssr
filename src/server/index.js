8627801a262819c884be7c4484e57afc9e915abc
dozer <diz.dozer@gmail.com>
2020-09-14 14:58:48 +0300

    wip
    wip
    
    wip
    
    wip
    
    wip
    
    wip

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,6 @@
+import server from 'fastify'
 import { renderToString } from 'inferno-server'
 import { inspect } from 'util'
-import server from 'fastify'
 import { App } from '../client'
 
 inspect.defaultOptions = { depth: null }
@@ -13,10 +13,14 @@ const app = server({
 })
 
 const initInfo = err =>
-  err ? console.error(err) : console.info(`${String.fromCharCode(9763)} ${APP_NAME} v.${APP_VERSION} : ${SERVER_PORT}`)
+  err
+    ? console.error(err)
+    : console.info(`${String.fromCharCode(9763)} ${APP_NAME} v.${APP_VERSION} : ${SERVER_PORT}`)
 
 const props = { color: 'red', name: 'Hello world' }
 
 const handler = (req, reply) => reply.type('text/html').send(renderToString(App(props)))
 
-app.get('/', handler).listen(Number(SERVER_PORT), String(SERVER_HOST), Number(SERVER_BACKLOG), initInfo)
+app
+  .get('/', handler)
+  .listen(Number(SERVER_PORT), String(SERVER_HOST), Number(SERVER_BACKLOG), initInfo)
