36eb52be9cec1ecb76fa0cc30c9d910cbd374d3a
dozer <diz.dozer@gmail.com>
2020-09-14 19:37:37 +0300

    wip

--- src/client/index.js
+++ src/client/index.js
@@ -2,4 +2,4 @@ const Item = ({ name }) => <li>Hello {name}</li>
 export const App = ({ style, name }) =>
   <ol $HasNonKeyedChildren style={style}>
     {Array.from({ length: 1000 }, () => Item({ name }))}
-  </ol>
+
