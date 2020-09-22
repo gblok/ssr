798e5fc561422762114a1a946ba76a997c9fd8e1
dozer <diz.dozer@gmail.com>
2020-09-22 10:15:02 +0300

    wip

--- src/server/routes/app/index.js
+++ src/server/routes/app/index.js
@@ -1,13 +1,13 @@
 import { Healthy, LoaderIo, LoaderIoToken, Main, SSR } from '../../controllers/app'
 
 export const App = (f, opts, done) => {
-  f.get('/', {}, Main)
+  f
+    .get('/', Main)
     .get('/:page', Main)
     .get('/:page/:sub', Main)
     .get('/:page/:sub/:lvl', Main)
-    .get('/healthy', {}, Healthy)
-    .get(`/${LoaderIoToken}`, {}, LoaderIo)
-    .get('/ssr', {}, SSR)
-  
+    .get('/healthy', Healthy)
+    .get(`/${LoaderIoToken}`, LoaderIo)
+    .get('/ssr', SSR)
   return done()
 }
